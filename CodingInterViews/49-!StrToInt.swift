c118eadcd5da5b3cf632e82528a6db79bd7c32a9
wymany <wymany@wyman.local>
2019-08-18 18:01:16 +0800

    添加了47不用加减乘除做加法的描述

---
+++ CodingInterViews/49-!StrToInt.swift
@@ -0,0 +1,12 @@
+//
+//  49-!StrToInt.swift
+//  CodingInterViews
+//
+//  Created by wymany on 2019/8/18.
+//  Copyright © 2019 wymany. All rights reserved.
+//
+
+import Foundation
+func strToInt(Str:[Character]) -> Int? {
+    return nil
+}
