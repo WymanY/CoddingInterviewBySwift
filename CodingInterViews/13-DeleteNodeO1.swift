e4899af528cb0e80d65d3079abba0dc7812414ce
wymany <wymany@wyman.local>
2019-08-18 15:48:00 +0800

    对解决的问题进行编号并添加13题在O1时间删除链表节点

---
+++ CodingInterViews/13-DeleteNodeO1.swift
@@ -0,0 +1,12 @@
+//
+//  13-DeleteNodeO1.swift
+//  CodingInterViews
+//
+//  Created by wymany on 2019/8/18.
+//  Copyright © 2019 wymany. All rights reserved.
+//
+
+import Foundation
+/*
+ 在O(1)时间内删除链表的节点
+ */
